8f6083b9399e8862c772852eecfcaf3795f8e6fa
Дамир Гайнанов <damirgaynanov@gmail.com>
2022-10-22 00:31:16 +0700

    Task_002_Done

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Блок запроса входных данных
+Console.WriteLine("Enter the first number");
+string number1 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter the second number");
+string number2 = Console.ReadLine() ?? "";
+
+// Блок подготовительный (перевод строки в числао
+int a = int.Parse(number1);
+int b = int.Parse(number2);
+
+// Блок операции (условие-сравнение двух чисел)
+if (a > b)
+{
+    Console.WriteLine("max=" + a);
+}
+else
+{
+    Console.WriteLine("max=" + b);
+}
