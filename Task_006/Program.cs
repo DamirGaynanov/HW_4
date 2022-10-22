4b089eb1acac211f3c0f1e3b21297dc93c0fc369
Дамир Гайнанов <damirgaynanov@gmail.com>
2022-10-22 15:39:53 +0700

    Task_006_Done

--- Task_006/Program.cs
+++ Task_006/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Requrst for input data
+Console.WriteLine("Enter the number");
+string number = Console.ReadLine() ?? "";
+
+// Processing of input data
+int a = int.Parse(number);
+
+// Decision
+if (a % 2 == 0) Console.WriteLine("Введённое число чётное");
+else Console.WriteLine("Введённое число нечётное");
+
+// Console.WriteLine(a % 2 == 0 ? "Чётное" : "Не чётное"); - Вариант решения из инетренета
