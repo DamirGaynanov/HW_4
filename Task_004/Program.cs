3103f5bcccd8f3454c7ac959c2ad5f4053d0183f
Дамир Гайнанов <damirgaynanov@gmail.com>
2022-10-22 14:52:18 +0700

    Task_004_done

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Requrst for input data
+Console.WriteLine("Enter the first number");
+string number_1 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter the second number");
+string number_2 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter the third number");
+string number_3 = Console.ReadLine() ?? "";
+
+// Processing of input data
+
+int a = int.Parse(number_1);
+int b = int.Parse(number_2);
+int c = int.Parse(number_3);
+
+// Decision
+int num_max = a;
+
+if (b > num_max)
+{
+    num_max = b;
+}
+if (c > num_max)
+{
+    num_max = c;
+}
+
+Console.WriteLine("max=" + num_max);
